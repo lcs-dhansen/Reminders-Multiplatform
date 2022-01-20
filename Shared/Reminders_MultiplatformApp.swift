761c441c0d35a02869f47109ed06630ae0222fbe
Devon Hansen <dhansen@lcs.on.ca>
2022-01-20 11:47:06 -0500

    Initial Commit

---
+++ Shared/Reminders_MultiplatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reminders_MultiplatformApp.swift
+//  Shared
+//
+//  Created by Devon Kenneth Hansen on 2022-01-20.
+//
+
+import SwiftUI
+
+@main
+struct Reminders_MultiplatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
