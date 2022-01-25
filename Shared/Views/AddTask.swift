7c4b0be386855faa32140cd8bc8fa1062a97aa4b
Devon Hansen <dhansen@lcs.on.ca>
2022-01-25 08:59:36 -0500

    Added Add Task view

---
+++ Shared/Views/AddTask.swift
@@ -0,0 +1,51 @@
+//
+//  AddTask.swift
+//  Reminders Multiplatform
+//
+//  Created by Devon Kenneth Hansen on 2022-01-25.
+//
+
+import SwiftUI
+
+struct AddTask: View {
+    
+    @State private var description = ""
+    @State private var priority = TaskPriority.low
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Form {
+                    TextField("description", text: $description)
+                    
+                    Picker("Priority", selection: $priority) {
+                        Text(TaskPriority.low.rawValue).tag(TaskPriority.low)
+                        Text(TaskPriority.medium.rawValue).tag(TaskPriority.medium)
+                        Text(TaskPriority.high.rawValue).tag(TaskPriority.high)
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+            }
+            .navigationTitle("New Reminder")
+            .toolbar {
+                ToolbarItem(placement: .primaryAction) {
+                    Button("Save") {
+                        saveTask()
+                    }
+                }
+            }
+            
+        }
+    }
+    
+    func saveTask() {
+        
+    }
+    
+}
+
+struct AddTask_Previews: PreviewProvider {
+    static var previews: some View {
+        AddTask()
+    }
+}
