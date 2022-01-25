c711f34bc26da8be1714bc0a209ecebdbb01047e
Devon Hansen <dhansen@lcs.on.ca>
2022-01-25 09:23:52 -0500

    added method for saving task to the task store

--- Shared/Views/AddTask.swift
+++ Shared/Views/AddTask.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct AddTask: View {
     
+    //Get a reference toi the store of tasks (TaskStore)
+    @ObservedObject var store: TaskStore
+    
     @State private var description = ""
     @State private var priority = TaskPriority.low
     
@@ -43,6 +46,9 @@ struct AddTask: View {
     
     func saveTask() {
         
+        //add the task to the task list
+        store.tasks.append(Task(description: description, priority: priority, completed: false))
+        
         //dismiss this view by changing bool to false
         showing = false
         
@@ -52,6 +58,6 @@ struct AddTask: View {
 
 struct AddTask_Previews: PreviewProvider {
     static var previews: some View {
-        AddTask(showing: .constant(true))
+        AddTask(store: testStore, showing: .constant(true))
     }
 }
--- Shared/Views/ContentView.swift
+++ Shared/Views/ContentView.swift
@@ -29,7 +29,7 @@ struct ContentView: View {
             }
         }
         .sheet(isPresented: $showingAddTask) {
-            AddTask(showing: $showingAddTask)
+            AddTask(store: store, showing: $showingAddTask)
         }
     }
 }
